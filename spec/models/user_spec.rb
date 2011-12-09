7d3dc70a888f67b0aa95558881ddf9b2a366fb97
Meng Fung <gem_one@hotmail.com>
2011-12-09 09:39:12 -0800

    removed view specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,11 +11,26 @@ describe User do
     User.first.should eq(user)
   end
   
-  it "should only allow 1 user to have a specific email address" do
+  it "should not allow duplicate email addresses" do
     user = User.create(@user_attr)
     user2 = User.new(@user_attr)
     user2.should_not be_valid
     user2.should have(1).error_on(:email)
     user2.errors[:email].should include("has already been taken")
+  end
+  
+  it "should be able to assign clients to users" do
+    user = FactoryGirl.build(:user)
+    app = FactoryGirl.build(:client)
+    user.clients << app
+    user.save
+    user.clients.should have(1).client
+  end
+  
+  it "should create a new user with its status as 'Active'" do
+    user = FactoryGirl.build(:user)
+    user.status = ''
+    user.save
+    user.status.should eq('Active')
   end
 end
