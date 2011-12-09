3d66e66c27b2701813bbbac4747f28982a071a17
Meng Fung <gem_one@hotmail.com>
2011-12-08 16:20:11 -0800

    finished testing client model

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -3,6 +3,7 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe Client do
   before :each do
     @client = FactoryGirl.build(:client)
+    @client_attr = FactoryGirl.attributes_for(:client)
   end
   
   it "should create a new client" do
@@ -18,4 +19,39 @@ describe Client do
     client.errors[:name].should include("can't be blank")
   end
   
+  it "should create an auto generated app_id 32 characters long (16 hex)" do
+    client = Client.create(@client_attr)
+    client.app_id.size.should == 32
+  end
+  
+  it "should create an auto generated app_secret 32 characters long (16 hex)" do
+    client = Client.create(@client_attr)
+    client.app_secret.size.should == 32
+  end
+  
+  it "should be able to filter a remote_roles_path and add / to beginning on update" do
+    client = Client.create(@client_attr)
+    client.update_attributes({:remote_roles_path => "api/roles"})
+    client.save
+    client.remote_roles_path.should eq("/api/roles")
+  end
+  
+  it "should be able to filter a remote_roles_path and add / to beginning on create" do
+    @client_attr[:remote_roles_path] = 'api/roles'
+    client = Client.create(@client_attr)
+    client.remote_roles_path.should eq("/api/roles")
+  end
+  
+  it "should return a string in the form of id: name for collections when name_with_id" do
+    client = Client.create(@client_attr)
+    client.name_with_id.should eq("#{client.id}: #{client.name}")
+  end
+  
+  it "should return the correct client model when I give it an app_id and app_secret" do
+    client = Client.create(@client_attr)
+    client.app_id = 'app_id'
+    client.app_secret = 'app_secret'
+    client.save
+    Client.authenticate('app_id', 'app_secret').should eq(client)
+  end
 end
